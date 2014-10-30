922f44dcbebd2d94d15f79e205423cecd7b0e0d3
Jozsef Kozma <jozsef.kozma@gmail.com>
2014-10-30 13:14:49 +0100

    valamivel jobb print style

--- js/app.js
+++ js/app.js
@@ -23,17 +23,20 @@
 				urls = 0,
 				that = this;
 
+			$('#because-print').html(_.map(this.data, function (line, topic) {
+				return '<span class="plm vpn mvn">' + line.text + '</span>';
+			}));
+
 			$('#because').html(_.map(this.data, function (line, topic) {
 				tags++;
-				var print = '<span class="print-only plm">' + line.text + '</span>';
 				if (line.urls === null || line.urls.length === 0) {
-					return $('<span class="hide-on-print">' + line.text + '</span>' + print);
+					return $('<span>' + line.text + '</span>');
 				} else {
 					urls += line.urls.length;
 					if (line.urls.length > 1) {
-						return $('<span class="hide-on-print"><a href="#" class="multi-url" data-topic="' + topic + '">' + line.text + '</a></span>' + print);
+						return $('<span><a href="#" class="multi-url" data-topic="' + topic + '">' + line.text + '</a></span>' + print);
 					} else {
-						return $('<span class="hide-on-print"><a href="' + line.urls[0] + '">' + line.text + '</a></span>' + print);
+						return $('<span><a href="' + line.urls[0] + '">' + line.text + '</a></span>' + print);
 					}
 				}
 			}));
